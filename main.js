7b5bacb3102b591e982b4836bec8c68559fe61e7
Konitch <56593997+Konitch@users.noreply.github.com>
2019-10-19 04:00:33 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
+const token = process.env.token
 
 client.on('ready', () => {
     console.log("Conectado como " + client.user.tag)
 })
 
-client.login('NDEyNjk0MzAyOTQ3NTQwOTk3.XaqyrA.oE9WbOBIpniPUFWBeoLi_8nvWAc')
-//vou criar um bot e pegar o token pera
+client.login(token).catch(err => console.log(err))
