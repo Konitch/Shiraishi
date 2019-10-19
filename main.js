b4ffeec33b26e74f20a497d1d4c20e85ae584c27
Konitch <56593997+Konitch@users.noreply.github.com>
2019-10-18 23:38:32 -0300

    Tchau

--- main.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
-const token = process.env.token
-
-client.on('Pronto!', () => {
-    console.log("Conectado como " + client.user.tag)
-})
-
-client.login(token).catch(err => console.log(err))
