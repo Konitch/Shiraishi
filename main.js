468cfb3ef418f723055319e606e14697cae909f0
Konitch <56593997+Konitch@users.noreply.github.com>
2020-11-24 22:08:09 -0300

    Update main.js

--- main.js
+++ main.js
@@ -6,17 +6,10 @@ client.on('ready', () => {
     console.log("Conectado como " + client.user.tag)
 })
 
-let guild = client.guilds.get('429299876212506624'), // returns a Guild or undefined
-    channel;
-
-    channel = guild.channels.get('780955663365963846');
-
-bot.on('message', async message => {
-    if(message.author.bot) return;
-    if(message.channel.type === "dm") {
-        client.guild.channel.send(user.message); 
-    }
-
-    });
+if(message.content.startsWith(`${prefix}dm`)) {
+    const mentionedUser = message.mentions.users.first();
+    if(!mentionedUser) return message.reply('Mention Somebody');
+    mentionedUser.send('Hi');
+}
 
 client.login(token).catch(err => console.log(err))
