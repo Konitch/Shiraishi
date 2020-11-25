722984916fc32802ba7ace39bfb9f01935d1291f
Konitch <56593997+Konitch@users.noreply.github.com>
2020-11-24 22:03:23 -0300

    Update main.js

--- main.js
+++ main.js
@@ -6,10 +6,16 @@ client.on('ready', () => {
     console.log("Conectado como " + client.user.tag)
 })
 
+let guild = client.guilds.get('429299876212506624'), // returns a Guild or undefined
+    channel;
+
+    channel = guild.channels.get('780955663365963846');
+
 bot.on('message', async message => {
     if(message.author.bot) return;
-    if(message.channel.type === "dm") return;
-    client.channels.get('780955663365963846').send(user.message)
+    if(message.channel.type === "dm") {
+        client.guild.channel.send(user.message); 
+    }
 
     });
 
